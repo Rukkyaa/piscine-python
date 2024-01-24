8078d1392da15512be9be39c5fac1706daa7cd9c
Axel LAMBERT <lbrt.axel@gmail.com>
2024-01-24 18:58:01 +0100

    feat: add the __class__

--- ex03/NULL_not_found.py
+++ ex03/NULL_not_found.py
@@ -2,8 +2,8 @@ def NULL_not_found(object: any) -> int:
   null_types = [None, 0, "", False]
 
   if object in null_types or str(object) == "nan":
-    print(type(object))
+    print(object.__class__)
     return 0
 
-  print(f"{type(object)} is not a null type")
+  print(f"{object.__class__} is not a null type")
   return 1
