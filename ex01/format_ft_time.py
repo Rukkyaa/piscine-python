43be5fdd91e25d31c50254458d650b196b1ca8e7
Axel LAMBERT <lbrt.axel@gmail.com>
2024-01-24 18:28:59 +0100

    feat: add the second exercice

---
+++ ex01/format_ft_time.py
@@ -0,0 +1,9 @@
+from time import time
+from datetime import datetime
+
+total_seconds = time()
+formatted_seconds = "{:,}".format(total_seconds)
+
+formatted_time = datetime.now().strftime("%b %d %Y")
+
+print(f"Seconds since January 1, 1970: {formatted_seconds} or {total_seconds:.2e} in scientific notation", formatted_time, sep="\n")
