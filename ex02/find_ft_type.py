5b868d10ba438df6ec59192949a78548163cdb0c
Axel LAMBERT <lbrt.axel@gmail.com>
2024-01-24 19:23:27 +0100

    feat: add the doc

--- ex02/find_ft_type.py
+++ ex02/find_ft_type.py
@@ -1,3 +1,13 @@
 def all_thing_is_obj(object: any) -> int:
+    """
+    This function is used to find the type of the object.
+
+    Parameters:
+        object (any): The object to be tested.
+
+    Returns:
+        int: 42
+    """
+
     print(object.__class__)
     return 42
--- ex03/NULL_not_found.py
+++ ex03/NULL_not_found.py
@@ -1,4 +1,15 @@
 def NULL_not_found(object: any) -> int:
+    """
+    This function is used to print the type of the object if it is a null type.
+    Otherwise, it will print that the object is not a null type.
+
+    Parameters:
+        object (any): The object to be tested.
+
+    Returns:
+        int: 0 if object is a null type, 1 otherwise.
+    """
+
     null_types = [None, 0, "", False]
 
     if object in null_types or str(object) == "nan":
